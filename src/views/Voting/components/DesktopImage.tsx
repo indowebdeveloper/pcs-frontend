1713ddf048d90435920254a1a76e7efdf358b71e
Indowebdeveloper <git@indowebdeveloper.com>
2021-09-26 23:29:39 +0700

    first

---
+++ src/views/Voting/components/DesktopImage.tsx
@@ -0,0 +1,12 @@
+import { Image } from '@dxswap/uikit'
+import styled from 'styled-components'
+
+const DesktopImage = styled(Image)`
+  display: none;
+
+  ${({ theme }) => theme.mediaQueries.lg} {
+    display: block;
+  }
+`
+
+export default DesktopImage
